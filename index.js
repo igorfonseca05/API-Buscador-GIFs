5a1a95c7c12025f2aef7725f28a4ed8013575ee1
Igor R da Fonseca <igorfonseca27@hotmail.com>
2023-06-19 16:54:06 -0300

    Projeto buscador de GIFs
    Neste projeto, crie um site simples que busca GIFS usando um API do  GIPHY

---
+++ index.js
@@ -0,0 +1,45 @@
+
+const form = document.querySelector('form')
+const out = document.querySelector('.out')
+
+const getGif = async (textInput) => {
+  try {
+    const response = await
+      fetch(`https://api.giphy.com/v1/gifs/search?api_key=q80WpgPaNzCH0RIELuI49xcNU6ikEWI0&limit=1&q=${textInput}`)
+
+    if (!response.ok) {
+      throw new Error('Não foi possível obter os dados')
+    }
+
+    return response.json()
+
+  } catch (error) {
+    console.log(error)
+  }
+
+}
+
+const showGifOnScreen = async (textInput) => {
+  const gif = await getGif(textInput);
+
+  if (gif) {
+    const image = document.createElement('img')
+    image.setAttribute('src', `${gif.data[0].images.downsized.url}`)
+    image.setAttribute('alt', `${gif.data[0].title}`)
+    out.insertAdjacentElement('afterbegin', image)
+  }
+
+}
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault()
+
+  const textInput = event.target.search.value;
+
+  if (textInput.length) {
+    getGif(textInput)
+    showGifOnScreen(textInput)
+  }
+
+  event.target.reset();
+})
